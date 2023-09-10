4bfb56c27ac014c0286022d469b91151748b1f65
Demah <hamedsedighi@gmail.com>
2023-09-10 20:08:20 +0200

    refactor rijks api

--- server/api/rijks.ts
+++ server/api/rijks.ts
@@ -3,6 +3,7 @@ import { requestRijksdata } from '~/utils/request-rijks-data';
 export default defineEventHandler(async (event) => {
   const query = getQuery(event);
   const param = query.param;
+
   const data = await requestRijksdata(param as string);
 
   return data;
