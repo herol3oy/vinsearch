b3246768047772b8fb5880fa6991454516377980
Demah <hamedsedighi@gmail.com>
2023-09-21 22:38:07 +0200

    style homepage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
   modules: ['@nuxt/image'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -9,13 +9,43 @@ const onSubmitForm = async () => {
 </script>
 
 <template>
-  <form @submit.prevent="onSubmitForm">
-    <input
-      v-model="userSearchQuery"
-      placeholder="Type a keyword e.g: Vincent van Gogh, nature, etc"
-    >
-    <button type="submit">
-      Search
-    </button>
-  </form>
+  <main class="flex justify-center items-center min-h-screen p-5">
+    <section class="flex flex-col gap-5">
+      <div class="flex flex-col justify-center items-center gap-5">
+        <img
+          class="w-40 h-4w-40"
+          src="~/assets/vincent.png"
+          alt="Vincent van Gogh"
+        >
+        <h1 class="text-5xl font-extrabold text-center">
+          VintSearch
+        </h1>
+        <p class="text-center text-2xl w-full md:w-3/4">
+          Vintsearch helps you to search through the artifacts of differnet
+          museums that provided public acesse to their API
+        </p>
+      </div>
+      <form
+        class="flex p-5 justify-center items-center flex-col md:flex-row gap-5"
+        @submit.prevent="onSubmitForm"
+      >
+        <input
+          v-model="userSearchQuery"
+          class="input w-full input-bordered input-primary max-w-xs"
+          placeholder="Type a keyword e.g: sky, bird"
+        >
+        <button
+          class="btn btn-primary"
+          type="submit"
+        >
+          Search
+        </button>
+      </form>
+      <small class="text-center">
+        Rijksmuseum • The Art Institute of Chicago • The Cleveland Museum of Art
+        <br>
+        and more are coming...
+      </small>
+    </section>
+  </main>
 </template>
